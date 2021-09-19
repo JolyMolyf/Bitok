8b7b40ad0c6c2ab47f380d27ac0c39a7eb8ba3f2
JolyMolyF <vs2001dor@gmail.com>
2021-09-20 02:32:00 +0300

    test reference

--- models/client.ts
+++ models/client.ts
@@ -8,7 +8,7 @@ module.exports = (sequelize:any, DataTypes:any) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models:any) {
-      // define association here
+      Client.belongsTo(models.Person)
     }
   };
   Client.init({
